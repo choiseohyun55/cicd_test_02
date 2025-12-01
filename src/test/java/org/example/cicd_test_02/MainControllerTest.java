acde468aaa3c92f27b6afce6ffb234d88c494bc3
choiseohyun55 <qkr7576@naver.com>
2025-12-01 12:02:02 +0900

    cicd test

---
+++ src/test/java/org/example/cicd_test_02/MainControllerTest.java
@@ -0,0 +1,13 @@
+package org.example.cicd_test_02;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MainControllerTest {
+    @Test
+    void main() {
+        Assertions.assertTrue(true);
+    }
+}
